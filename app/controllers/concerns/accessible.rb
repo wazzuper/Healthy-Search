da80d4b52e0a38364e4438c9f0b7f45c9330fb24
artem <clifredd@gmail.com>
2018-02-15 21:42:24 +0500

    Configured devise gem

---
+++ app/controllers/concerns/accessible.rb
@@ -0,0 +1,17 @@
+module Accessible
+  extend ActiveSupport::Concern
+  included do
+    before_action :check_user
+  end
+
+  protected
+  def check_user
+    if current_doctor
+      flash.clear
+      redirect_to(authenticated_doctor_root_path) && return
+    elsif current_patient
+      flash.clear
+      redirect_to(authenticated_patient_root_path) && return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :patients, path: 'patients', controllers: {
+    sessions: 'patients/sessions',
+    registrations: 'patients/registrations',
+    passwords: 'patients/passwords'
+  }
+
+  # devise_scope :patient do
+  #   get 'sign_in', to: 'patients/sessions#new'
+  # end
+
+  devise_for :doctors, path: 'doctors', controllers: {
+    sessions: 'doctors/sessions',
+    registrations: 'doctors/registrations',
+    passwords: 'doctors/passwords'
+  }
+
+  # devise_scope :doctor do
+  #   get 'sign_in', to: 'doctors/sessions#new'
+  # end
 end
