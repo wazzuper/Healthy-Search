bc1805132f1b91ee71bb90dcf4de2422a57a3e3d
artem <clifredd@gmail.com>
2018-02-16 13:57:10 +0500

    updated migrations, modified edit pages

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20180215172626) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "full_name"
-    t.string "phone"
+    t.string "phone_number"
     t.index ["email"], name: "index_patients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
   end
