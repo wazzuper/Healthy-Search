1899a0def14b6e3434938818635fd04e5b86fa68
Artem Korotchenko <clifredd@gmail.com>
2018-02-21 19:32:59 +0500

    Doctor can update his address

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -11,6 +11,9 @@ class DoctorsController < ApplicationController
   def bio
   end
 
+  def address
+  end
+
   def update
     if @doctor.update(doctor_params)
       redirect_to edit_doctor_registration_path(@doctor)
@@ -29,6 +32,6 @@ class DoctorsController < ApplicationController
     params.require(:doctor).permit(:full_name, :phone_number, :email, :experience, :bio,
                                     :is_cardiologist, :is_dermatologist, :is_endocrinologist,
                                     :is_gastroenterologist, :is_oncologist, :is_ophthalmologist,
-                                    :is_pediatrician, :is_physiatrist)
+                                    :is_pediatrician, :is_physiatrist, :address)
   end
 end
