94933c1c9c04de87792ba88217c7734d2bf1977b
Artem Korotchenko <clifredd@gmail.com>
2018-03-05 16:05:50 +0500

    recovering from the git fiasko #2 - Appointment, association, mirgration, cotnroller

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,11 @@
+class AppointmentsController < ApplicationController
+  # before_action :authenticate!  
+
+  # private
+
+  # def authenticate!
+  #   unless doctor_signed_in? || patient_signed_in?
+  #     redirect_to :login_page
+  #   end
+  # end
+end
