5e596a1a77575f48856349e9c3bde7c79c979834
Artem Korotchenko <clifredd@gmail.com>
2018-03-26 12:12:15 +0500

    Added filter for search

--- app/controllers/specializations_controller.rb
+++ app/controllers/specializations_controller.rb
@@ -1,9 +1,11 @@
 class SpecializationsController < ApplicationController
   def index
-    if params[:search].eql?('')
+    @filter = params[:search]
+
+    if @filter.eql?('')
       @doctors = Doctor.all
     else
-      @specialization = Specialization.find_by_name(params[:search])
+      @specialization = Specialization.find_by_name(@filter)
     end
   end
 end
