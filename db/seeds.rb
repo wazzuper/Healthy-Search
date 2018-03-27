e879db75cedb44bde5919c1b62e9ffbedaa06a21
Artem Korotchenko <clifredd@gmail.com>
2018-03-27 13:57:28 +0500

    Beautification for doctors cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,24 @@ require 'open-uri'
 #   Specialization.create(name: word)
 # end
 
-200.times do
-  spec_id = rand(106..157)
-  exp = rand(1..45)
-  price = rand(15..30) if exp >= 1 || exp < 5
-  price = rand(25..45) if exp >= 5 || exp < 15
-  price = rand(40..60) if exp >= 15 || exp < 30
-  price = rand(55..80) if exp >= 30 || exp <= 45
+# 200.times do
+#   spec_id = rand(106..157)
+#   exp = rand(1..45)
+#   if exp >= 1 && exp < 5
+#     price = rand(15..30)
+#   elsif exp >= 5 && exp < 15
+#     price = rand(25..45)
+#   elsif exp >= 15 && exp < 30
+#     price = rand(40..60)
+#   else
+#     price = rand(55..80)
+#   end
 
-  Doctor.create(full_name: Faker::Name.name,
-                email: Faker::Internet.safe_email,
-                password: '123456',
-                specialization_id: spec_id,
-                experience: exp,
-                price: price,
-                phone_number: Faker::PhoneNumber.cell_phone)
-end
+#   Doctor.create(full_name: Faker::Name.name,
+#                 email: Faker::Internet.safe_email,
+#                 password: '123456',
+#                 specialization_id: spec_id,
+#                 experience: exp,
+#                 price: price,
+#                 phone_number: Faker::PhoneNumber.phone_number)
+# end
