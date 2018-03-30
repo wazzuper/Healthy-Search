657e193e7e06d923d8e499633073a2dc4560ea9c
Artem Korotchenko <clifredd@gmail.com>
2018-03-30 16:30:56 +0500

    Modified devise registrations controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :specialization_id])
-    devise_parameter_sanitizer.permit(:account_update, keys:
-      %i(full_name phone_number avatar avatar_cache remove_avatar))
-  end
 end
