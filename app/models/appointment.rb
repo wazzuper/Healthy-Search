abcf678d9731f695a653d278cada26ddba723f0f
Artem Korotchenko <clifredd@gmail.com>
2018-03-31 19:25:13 +0500

    find_booked_time method refactoring

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -13,7 +13,7 @@ class Appointment < ApplicationRecord
 
   def self.find_booked_time(doctor, date)
     list_of_hours = []
-    Appointment.joins(:patient).where(doctor: doctor).where(date: date).each do |a|
+    Appointment.joins(:patient, :doctor).where(date: date).each do |a|
       list_of_hours << a.visiting_hour_id
     end
     list_of_hours
