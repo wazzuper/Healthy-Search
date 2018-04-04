f1daec3aecd9d9398996ff671fd1205ba9a2209f
Artem Korotchenko <clifredd@gmail.com>
2018-04-04 19:27:35 +0500

    Added validation, condition and locales for Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
   belongs_to :appointment
+
+  validates :rating, presence: true
+  validates :comment, presence: true
+  validates :appointment_id, uniqueness: { scope: :appointment_id }
 end
