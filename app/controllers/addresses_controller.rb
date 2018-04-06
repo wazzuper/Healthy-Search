aaaa40c09896eb1b38896871b63be4a2af913fe2
Artem Korotchenko <clifredd@gmail.com>
2018-04-06 12:38:27 +0500

    Added addresses contoller and #create, #update actions

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,36 @@
+class AddressesController < ApplicationController
+  before_action :set_doctor
+  before_action :authenticate_doctor!
+
+  def create
+    address = @doctor.create_address(address_params)
+
+    if address.save
+      flash[:notice] = 'Address added'
+    else
+      flash[:alert] = address.errors.full_messages.join(', ')
+    end
+
+    redirect_back(fallback_location: request.referer)
+  end
+
+  def update
+    if @doctor.address.update_attributes(address_params)
+      flash[:notice] = 'Address updated'
+    else
+      flash[:alert] = address.errors.full_messages.join(', ')
+    end
+
+    redirect_back(fallback_location: request.referer)
+  end
+
+  private
+
+  def set_doctor
+    @doctor ||= Doctor.find(params[:doctor_id])
+  end
+
+  def address_params
+    params.require(:address).permit(:place)
+  end
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :doctor
 
+  geocoded_by :place
+  after_validation :geocode, if: :place_changed?
+
   validates :doctor_id, uniqueness: { scope: :doctor_id }
 end
