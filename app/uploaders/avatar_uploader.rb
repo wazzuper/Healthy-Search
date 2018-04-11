fb22c5c92bc6f8f39d3cc2a7f5d88ecc06431c05
Artem Korotchenko <clifredd@gmail.com>
2018-04-12 03:14:00 +0500

    Added devise messages and whitelist for uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,4 +4,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png svg)
+  end
 end
