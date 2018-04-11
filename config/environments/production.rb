32ca297b17df78f13055217b9f9ebd3e9d1445c1
Artem Korotchenko <clifredd@gmail.com>
2018-04-11 16:41:56 +0500

    env keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.exceptions_app = self.routes
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
