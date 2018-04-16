9243229a4d85efc6b6969e5efd4e3811bd9a3408
Artem Korotchenko <clifredd@gmail.com>
2018-04-16 11:46:11 +0500

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ require 'open-uri'
 #   Specialization.create(name: word)
 # end
 
+# Create 200 doctors with random parameters
 # 200.times do
 #   spec_id = rand(1..Specialization.all.count)
 #   exp = rand(1..45)
@@ -45,21 +46,38 @@ require 'open-uri'
 #   d.save
 # end
 
+# Patient for testing
 # Patient.create(full_name: Faker::Name.name,
 #   email: Faker::Internet.safe_email,
 #   password: '123456',
 #   phone_number: Faker::PhoneNumber.phone_number)
 
+# Review with random rating
+# Doctor.all.each do |doctor|
+#   v = VisitingDay.create(date: Date.today, doctor_id: doctor.id)
+#   VisitingHour.create(time: Time.now, visiting_day_id: v.id)
+
+#   Appointment.create(doctor_id: doctor.id, patient_id: Patient.first.id,
+#     visiting_day_id: doctor.visiting_days.first.id, visiting_hour_id: doctor.visiting_days.first.visiting_hours.first.id,
+#     date: doctor.visiting_days.first.date)
+# end
+
+# Appointment.all.each do |a|
+#   Review.create(rating: rand(1..5), comment: 'Random comment and rating', patient_id: a.patient_id, doctor_id: a.doctor_id,
+#     appointment_id: a.id)
+# end
+
+# Visiting hours and days
 Doctor.all.each do |doctor|
-  v = VisitingDay.create(date: Date.today, doctor_id: doctor.id)
-  VisitingHour.create(time: Time.now, visiting_day_id: v.id)
+  count = 0
 
-  Appointment.create(doctor_id: doctor.id, patient_id: Patient.first.id,
-    visiting_day_id: doctor.visiting_days.first.id, visiting_hour_id: doctor.visiting_days.first.visiting_hours.first.id,
-    date: doctor.visiting_days.first.date)
-end
-
-Appointment.all.each do |a|
-  Review.create(rating: rand(1..5), comment: 'Random comment and rating', patient_id: a.patient_id, doctor_id: a.doctor_id,
-    appointment_id: a.id)
+  7.times do
+    v = VisitingDay.create(date: Date.today + count, doctor_id: doctor.id)
+    VisitingHour.create(time: '09:00', visiting_day_id: v.id)
+    VisitingHour.create(time: '10:00', visiting_day_id: v.id)
+    VisitingHour.create(time: '11:00', visiting_day_id: v.id)
+    VisitingHour.create(time: '12:00', visiting_day_id: v.id)
+    VisitingHour.create(time: '13:00', visiting_day_id: v.id)
+    count += 1
+  end
 end
