4cb159410fe8b5e9f7873e37ec96583b864be88a
Artem Korotchenko <clifredd@gmail.com>
2018-04-18 14:40:20 +0500

    Added test for visiting hours method

--- spec/factories/visiting_hours.rb
+++ spec/factories/visiting_hours.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :visiting_hour do
-    time Time.now
+    time do
+      time = Time.now
+      time = Time.now + 3600 if VisitingHour.exists?(time: time)
+      time
+    end
     visiting_day
   end
 end
--- spec/models/visiting_hour_spec.rb
+++ spec/models/visiting_hour_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe VisitingHour, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#availabe_hours' do
+    let(:visiting_day_first) { create(:visiting_day) }
+    let(:visiting_day_second) { create(:visiting_day) }
+    before { create_list(:visiting_hour, 5, visiting_day_id: visiting_day_first.id) }
+    before { create_list(:appointment, 3, visiting_day_id: visiting_day_second.id) }
+    
+    it 'returns available hours for booking' do   
+      expect(visiting_day_first.visiting_hours.availabe_hours.count).to eq(5)
+    end
+
+    it 'not returns available hours for booking' do   
+      expect(visiting_day_second.visiting_hours.availabe_hours.count).to eq(0)
+    end
+  end
 end
