048008bf804a26f1b565fa519f5155921ea28ce3
Artem Korotchenko <clifredd@gmail.com>
2018-04-19 13:55:52 +0500

    Added mailer spec with #notification_email_for_doctor test

---
+++ spec/mailers/notification_mailer_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe NotificationMailer, type: :mailer do
+  describe '#notification_email_for_doctor' do
+    let(:appointment) { create(:appointment) }
+    let(:mail) { NotificationMailer.notification_email_for_doctor(appointment.doctor, appointment) }
+
+    it 'renders the headers' do
+      expect(mail.from).to eq(['no-reply@healthy-search.com'])
+      expect(mail.subject).to eq('Patient make an appointment!')
+      expect(mail.to).to eq([appointment.doctor.email])
+    end
+
+    it 'renders the doctor\'s name' do
+      expect(mail.body.encoded).to match(appointment.doctor.full_name)
+    end
+
+    it 'renders the patient\'s name' do
+      expect(mail.body.encoded).to match(appointment.patient.full_name)
+    end
+
+    it 'renders the appointment\'s url' do
+      expect(mail.body.encoded).to match("http://localhost:3000/appointments/#{appointment.id}")
+    end
+  end
+end
