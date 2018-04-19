1c1711b77af7675d4eae512befc29662508bcbd1
Artem Korotchenko <clifredd@gmail.com>
2018-04-19 16:42:39 +0500

    Added method for patients to NotificationMailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,4 +4,10 @@ class NotificationMailer < ApplicationMailer
     @appointment = appointment
     mail(to: @doctor.email, subject: I18n.t('mailer.make_appointment'))
   end
+
+  def notification_email_for_patient(patient, appointment)
+    @patient = patient
+    @appointment = appointment
+    mail(to: @patient.email, subject: 'Tommorow you have a meeting with a doctor!')
+  end
 end
