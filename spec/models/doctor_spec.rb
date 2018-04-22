395b1d034283f5bf541f0e6651beaf2ab818cf0d
Artem Korotchenko <clifredd@gmail.com>
2018-04-22 23:57:25 +0500

    Modified tests

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  before do
-    FactoryBot.create_list(:review, 10)
-  end
-  
+  let(:doctor) { create(:doctor) }
+  before { create_list(:review, 10) }
+
   describe '#three_best_doctors' do
+    subject(:best_three_doctors) { Doctor.three_best_doctors }
+
     it 'returns 3 doctors with highest rating' do
-      expect(Doctor.three_best_doctors.length).to eq(3)
-      expect(Doctor.three_best_doctors.first.average_rating).to be_between(4, 5)
+      expect(best_three_doctors.length).to eq(3)
+      expect(best_three_doctors.first.average_rating).to be_between(4, 5)
+      expect(best_three_doctors.second.average_rating).to be_between(4, 5)
+      expect(best_three_doctors.last.average_rating).to be_between(4, 5)
     end
+
+    it { is_expected.to_not include(doctor) }
   end
 end
